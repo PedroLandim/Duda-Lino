4adba4ebc8ee54d0b8222f8956c5e18101acfa5c
Pedro Landim <86279678+PedroLandim@users.noreply.github.com>
2024-12-23 15:53:28 -0300

    terminando detalhes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const { differenceInMonths, differenceInDays, addMonths } = dateFns;
 
-  const startDate = new Date("2024-08-15T23:59:59");
+  const startDate = new Date("2024-07-15T23:59:59");
   const now = new Date();
 
   // Cálculo dos meses
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Lista de imagens
   const imageDetails = [
-    { src: "img/img1.jpeg", date:"16/01/2024", text: "Nossa primeira foto juntos" },
+    { src: "img/img1.jpeg", date:"15 de Maio 2024", text: "Nossa primeira foto juntos, no Laguinho" },
     { src: "img/img2.jpeg", text: "Lembra dessa viagem incrível?" },
     { src: "img/img3.jpeg", text: "Essa foi uma noite especial para nós dois." },
     { src: "img/img4.jpeg", text: "Um dia cheio de risadas e diversão." },
@@ -22,10 +22,9 @@ document.addEventListener("DOMContentLoaded", function () {
     { src: "img/img6.jpeg", text: "Nosso primeiro aniversário juntos!" },
     { src: "img/img7.jpeg", text: "Nosso primeiro aniversário juntos!" },
     { src: "img/img8.jpeg", text: "Nosso primeiro aniversário juntos!" },
-    { src: "img/img9.jpeg", text: "Nosso primeiro aniversário juntos!" },
-    { src: "img/img10.jpeg", text: "Nosso primeiro aniversário juntos!" },
-    { src: "img/img11.jpeg", text: "Nosso primeiro aniversário juntos!" },
-    { src: "img/img12.jpeg", text: "Nosso primeiro aniversário juntos!" },
+    { src: "img/img9.jpeg", date: "28 de Junho 2024", text: "Reabrindo o pagode do Didi" },
+    { src: "img/img11.jpeg", date: "29 de Junho 2024", text: "Ela achando que iamos ficar milinários, mal sabia ela que eu já tinha ganhado na loteria" },
+    { src: "img/img12.jpeg", date: "29 de Junho 2024", text: "Primeira viagem para longe, Porto de Galinhas (eu, a Gatinha, Laís e Nicolly)" },
     { src: "img/img13.jpeg", text: "Nosso primeiro aniversário juntos!" },
     { src: "img/img14.jpeg", text: "Nosso primeiro aniversário juntos!" },
     { src: "img/img15.jpeg", text: "Nosso primeiro aniversário juntos!" },
@@ -46,12 +45,12 @@ document.addEventListener("DOMContentLoaded", function () {
     { src: "img/img30.jpeg", text: "Nosso primeiro aniversário juntos!" },
     { src: "img/img31.jpeg", text: "Nosso primeiro aniversário juntos!" },
     { src: "img/img32.jpeg", text: "Nosso primeiro aniversário juntos!" },
-    { src: "img/img33.jpeg", text: "Nosso primeiro aniversário juntos!" },
-    { src: "img/img34.jpeg", text: "Nosso primeiro aniversário juntos!" },
-    { src: "img/img35.jpeg", text: "Nosso primeiro aniversário juntos!" },
-    { src: "img/img36.jpeg", text: "Nosso primeiro aniversário juntos!" },
-    { src: "img/img37.jpeg", text: "Nosso primeiro aniversário juntos!" },
-    { src: "img/img38.jpeg", text: "Nosso primeiro aniversário juntos!" },
+    { src: "img/img33.jpeg", date: "1 de Junho 2024", text: "Provando Lingua de pato Bleeeergh" },
+    { src: "img/img34.jpeg", date: "31 de Maio 2024", text: "Nosso primeiro Barchefe! (infelizmente os preços pipocaram pouco depois)" },
+    { src: "img/img35.jpeg", date: "8 de Junho 2024", text: "Uma maloqueira pulando o muro da federal. Despedida do Peludo" },
+    { src: "img/img36.jpeg", date: "31 de Maio 2024", text: "Dois tabacudos testando óculos da Gucci" },
+    { src: "img/img37.jpeg", date: "27 de Junho 2024", text: "Indo ali assistir divertidamente (nem namorava e já tirava ft de casal)" },
+    { src: "img/img38.jpeg", date: "27 de Maio 2024", text: "Segunda vez no Barchefe, esse a gnt aproveitou bastante o Clericot" },
     { src: "img/img39.jpeg", text: "Nosso primeiro aniversário juntos!" },
     { src: "img/img40.jpeg", text: "Nosso primeiro aniversário juntos!" },
     { src: "img/img41.jpeg", text: "Nosso primeiro aniversário juntos!" },
